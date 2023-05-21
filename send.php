13e7fc43015dcdb17ebfc4581b2966eda4e92aba
ilupinka <128067372+ilupinka@users.noreply.github.com>
2023-05-21 19:18:17 +0300

    Update send.php

--- send.php
+++ send.php
@@ -1,44 +1,35 @@
 <?php
-$mero = $_POST['mero'];
-$secondname = $_POST['secondname'];
-$firstname = $_POST['firstname'];
-$otche = $_POST['otche'];
-$telephone = $_POST['telephone'];
-$pochta = $_POST['pochta'];
-$vardate = $_POST['vardate'];
+if (isset($_POST['mero'])) {$mero = $_POST['mero'];}
+if (isset($_POST['secondname'])) {$secondname = $_POST['secondname'];}
+if (isset($_POST['firstname'])) {$firstname = $_POST['firstname'];}
+if (isset($_POST['otche'])) {$otche = $_POST['otche'];}
+if (isset($_POST['telephone'])) {$telephone = $_POST['telephone'];}
+if (isset($_POST['pochta'])) {$pochta = $_POST['pochta'];}
+if (isset($_POST['vardate'])) {$vardate = $_POST['vardate'];}
 
+$myaddres = "apostolandrey2002@gmail.com";
 
-$mero = htmlspecialchars($mero);
-$secondname = htmlspecialchars($secondname);
-$firstname = htmlspecialchars($firstname);
-$otche = htmlspecialchars($otche);
-$telephone = htmlspecialchars($telephone);
-$pochta = htmlspecialchars($pochta);
-$vardate = htmlspecialchars($vardate);
+$mes = "Тема: Заказ мероприятия\nТелефон: $telephone\nИмя: $firstname;
 
-$mero = urlencode($mero);
-$secondname = urlencode($secondname);
-$firstname = urlencode($firstname);
-$otche = urlencode($otche);
-$telephone = urlencode($telephone);
-$pochta = urlencode($pochta);
-$vardate = urlencode($vardate);
+$sub='Заказ';
+$email='Заказ мероприятия';
+$send = mail ($myaddres,$sub,$mes,"Content-type:text/plain; charset = utf-8\r\nFrom:$email");
 
-$mero = trim($mero);
-$secondname = trim($secondname);
-$firstname = trim($firstname);
-$otche = trim($otche);
-$telephone = trim($telephone);
-$pochta = trim($pochta);
-$vardate = trim($vardate);
-
-if (mail("apostolandrey2002@gmail.com", "Тема", "Имя:".$firstname, .$secondname, .$otche, .$mero, .$telephone, .$vardate.". E-mail:".$pochta , "From: localhost@mail.ru \r\n"))
-){
-     echo ("Сообщение успешно доставлено!");
-}
-
-else {
-     echo ("При отправке возникла ошибка.");
-}
-
+ini_set('short_open_tag', 'On');
+header('Refresh: 3; URL=index.html');
 ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="refresh" content="3; url=index.html">
+<title>Благодарим за заказ. Скоро мы свяжемся с вами!</title>
+<meta name="generator">
+<script type="text/javascript">
+setTimeout('location.replace("/index.html")', 3000);
+</script> 
+</head>
+<body>
+<h1>Благодарим за заказ. Скоро мы свяжемся с вами!</h1>
+</body>
+</html>
